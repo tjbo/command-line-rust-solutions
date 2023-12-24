788a97fd73e937edeb02f3e90937a2a9506f0932
tjbo <tom@prototypable.ca>
2023-12-24 15:17:16 -0500

    most tests working for catr

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,5 +1,7 @@
-use std::error::Error;
 use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug)]
 pub struct Config {
@@ -8,52 +10,74 @@ pub struct Config {
     number_nonblank_lines: bool,
 }
 
+pub fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
 
 pub fn get_args() -> MyResult<Config> {
     let matches = App::new("catr")
-    .version("0.1.0")
-    .arg(
-        Arg::with_name("files")
-        .value_name("FILES")
-        .help("Input file(s)")
-        .multiple(true)
-        .default_value("-")
-    )
-    .arg(
-        Arg::with_name("number")
-        .short("n")
-        .long("number")
-        .help("Number lines")
-        .takes_value(false)
-        .conflicts_with("number_nonblank")
-    )
-    .arg(Arg::with_name("number_nonblank")
-        .short("b")
-        .long("Number non-blank")
-        .help("Number non-blank lines") 
-        .takes_value(false))
-    .author("tom")
-    .about("Rust cat")
-    .get_matches();
+        .version("0.1.0")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .takes_value(false),
+        )
+        .author("tom")
+        .about("Rust cat")
+        .get_matches();
 
     Ok(Config {
         files: matches.values_of_lossy("files").unwrap(),
         number_lines: matches.is_present("number"),
         number_nonblank_lines: matches.is_present("number_nonblank"),
     })
-
 }
-
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn run(config: Config) -> MyResult<()> {
-
+    let mut last_num = 0;
     for filename in config.files {
-        println!("{}", filename)
+        match open(&filename) {
+            Err(e) => eprintln!("Failed to open {}:{}", filename, e),
+            Ok(file) => {
+                for (line_num, line_result) in file.lines().enumerate() {
+                    let line = line_result?;
+                    if config.number_lines {
+                        println!("{:6}\t{}", line_num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
     }
-
-
-
     Ok(())
 }

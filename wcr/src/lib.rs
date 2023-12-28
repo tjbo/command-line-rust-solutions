5d3faf33e96e5dce6fc183c378f11e453e999afb
tjbo <tom@prototypable.ca>
2023-12-28 18:27:06 -0500

    more setup

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -13,17 +13,37 @@ pub struct Config {
 }
 
 pub fn get_args() -> MyResult<Config> {
-    // let matches = App::new("wcr")
-    //     .version("0.1.0")
-    //     .author("tjbo")
-    //     .about("Rust wc")
-    //     .get_matches()
+    let matches = App::new("wcr")
+        .version("0.1.0")
+        .author("tjbo")
+        .about("Rust wc")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .takes_value(false)
+                .help("Show byte count"),
+        )
+        .get_matches();
 
     Ok(Config {
-        files: vec![],
+        files: matches.values_of_lossy("files").unwrap(),
         lines: false,
         words: false,
-        bytes: false,
+        bytes: matches.is_present("bytes"),
         chars: false,
     })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
 }

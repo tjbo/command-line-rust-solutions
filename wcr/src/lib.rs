deb2b7b90655a73568be6f6c81aa255bb7f675df
tjbo <tom@prototypable.ca>
2023-12-28 19:17:13 -0500

    add cli logic

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -32,14 +32,43 @@ pub fn get_args() -> MyResult<Config> {
                 .takes_value(false)
                 .help("Show byte count"),
         )
+        .arg(
+            Arg::with_name("chars")
+                .short("m")
+                .long("chars")
+                .value_name("CHARS")
+                .takes_value(false)
+                .help("Show character count"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("l")
+                .long("lines")
+                .value_name("LINES")
+                .takes_value(false)
+                .help("Show line count"),
+        )
+        .arg(
+            Arg::with_name("words")
+                .short("w")
+                .long("words")
+                .value_name("WORDS")
+                .takes_value(false)
+                .help("Show word count"),
+        )
         .get_matches();
+
+    let is_default = !matches.is_present("words")
+        && !matches.is_present("lines")
+        && !matches.is_present("chars")
+        && !matches.is_present("bytes");
 
     Ok(Config {
         files: matches.values_of_lossy("files").unwrap(),
-        lines: false,
-        words: false,
-        bytes: matches.is_present("bytes"),
-        chars: false,
+        lines: is_default == true && true || matches.is_present("lines"),
+        words: is_default == true && true || matches.is_present("words"),
+        bytes: is_default == true && true || matches.is_present("bytes"),
+        chars: matches.is_present("chars"),
     })
 }
 

09a9174adbab76f24fe93ac489feb32c08c45b78
tjbo <tom@prototypable.ca>
2023-12-28 19:31:57 -0500

    add open

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -63,7 +65,7 @@ pub fn get_args() -> MyResult<Config> {
     let mut bytes = matches.is_present("bytes");
     let chars = matches.is_present("chars");
 
-    if [lines, words, bytes, chars].iter().all(|v| v == &false) {
+    if [lines, words, bytes, chars].iter().all(|v| !v) {
         lines = true;
         words = true;
         bytes = true;
@@ -81,4 +83,11 @@ pub fn get_args() -> MyResult<Config> {
 pub fn run(config: Config) -> MyResult<()> {
     println!("{:#?}", config);
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
